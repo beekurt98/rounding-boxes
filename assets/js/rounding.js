ceca7bb283626ba37fd83a5a1c183322c0629a5b
Berna Kurt <beekurt98@gmail.com>
2025-04-26 12:25:03 +0300

    add fixed heights and widths to rows and columns

--- assets/js/rounding.js
+++ assets/js/rounding.js
@@ -3,26 +3,25 @@ function randomNum(min, max) {
 }
 
 function boxRadius(element) {
-  $(element).css("border-radius", `${randomNum(10,100)}px ${randomNum(10,100)}px ${randomNum(10,100)}px ${randomNum(10,100)}px`)
+  $(element).css("border-radius", `${randomNum(10,100)}px ${randomNum(10,100)}px ${randomNum(10,100)}px ${randomNum(10,100)}px`);
 }
 
 function boxBorder(element) {
-  $(element).css("border-style", "solid")
-  $(element).css("border-color", `rgb(${randomNum(150,255)},${randomNum(150,255)},${randomNum(150,255)})`)
-  $(element).css("border-width", `${randomNum(1,3)}px ${randomNum(0,5)}px ${randomNum(1,3)}px ${randomNum(0,5)}px`)
-  $(element).css("transition", "0.4s")
+  $(element).css("border-style", "solid");
+  $(element).css("border-color", `rgb(${randomNum(150,255)},${randomNum(150,255)},${randomNum(150,255)})`);
+  $(element).css("border-width", `${randomNum(1,3)}px ${randomNum(0,5)}px ${randomNum(1,3)}px ${randomNum(0,5)}px`);
+  $(element).css("transition", "0.4s");
 }
-var randomBox = randomNum(0,8)
+var randomBox = randomNum(0,8);
 function both(element) {
-  boxBorder(element)  
-  boxRadius(element)
-  console.log(element)
+  boxBorder(element);
+  boxRadius(element);
 }
 
 function randomExecution(callback) {
-  const min = 1
-  const max = 3
-  var random = Math.floor(Math.random() * (max - min + 1) + min)
+  const min = 1;
+  const max = 3;
+  var random = Math.floor(Math.random() * (max - min + 1) + min);
 
   setTimeout(function() {
       callback();
@@ -31,17 +30,17 @@ function randomExecution(callback) {
 }
 
 function getRandomBox() {
-  var boxNum = document.querySelectorAll(".box").length
-  element = `.box:eq(${randomNum(0,boxNum)})`
-  both(element)
+  var boxNum = document.querySelectorAll(".box").length;
+  element = `.box:eq(${randomNum(0,boxNum)})`;
+  both(element);
 }
 
 function allBoxesRandom() {
-  var boxLen = document.querySelectorAll(".box").length
+  var boxLen = document.querySelectorAll(".box").length;
   setTimeout(() => {
       {for (i=0;i<boxLen;i++) {
-          element = `.box:eq(${i})`
-          both(element)
+          element = `.box:eq(${i})`;
+          both(element);
           // boxRadius(element)
       }}
   }, 1000);
@@ -49,12 +48,12 @@ function allBoxesRandom() {
 
 $(document).ready(function(){
   $(".box").hover(function(){
-      both(this)
+      both(this);
   }, function(){
-      both(this)
+      both(this);
   });
 });
 
-allBoxesRandom() 
-randomExecution(getRandomBox)
-randomExecution(getRandomBox)
+allBoxesRandom();
+randomExecution(getRandomBox);
+randomExecution(getRandomBox);
